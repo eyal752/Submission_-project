4b3f1e2ba9a86577cd5747a4dbbf96014ff80025
eyal752 <75213086+eyal752@users.noreply.github.com>
2020-12-31 15:57:29 +0200

    Create project.java

---
+++ project.java
@@ -0,0 +1,278 @@
+
+      //question 4:
+        //int question_0 =5;
+        //int question_1 = 20+12;
+        //float question_3 =7.5f;
+
+        //question 5
+
+     // int a,b ;
+
+     //  int sum ;
+     //  sum = a+b;
+      //  int multipy;
+       //  multipy = a * b ;
+
+
+          //question 6
+        //String color = "blue";
+
+
+        //question 7
+
+         //String name =s.nextLine() ;
+        //System.out.println("Hello" + " " + name + ", " + " " +"Welcome to java!"  );
+
+        //question 8
+
+       // int year = 220 ;
+       // year +=  10 ;
+       // year -=  30;
+
+      // System.out.println(year);
+
+        //question 9
+
+        /*System.out.println("Please enter a number  :");
+      int num1 = s.nextInt();
+        System.out.println("Please enter a number :");
+       int num2 = s.nextInt();
+        System.out.println("Please enter a number:");
+        int num3 = s.nextInt() ;
+        double average = (num1 +num2 +num3)/3;
+        System.out.println("average:"+ average );
+       /*
+        //question 10
+
+       // System.out.println("Your travel speed?");
+
+       // int speed = s.nextInt();
+
+       //if (speed >= 120) {
+
+              //  System.out.println("too fast ! ! !");
+
+      //  }
+
+      //  if (speed <80 ){
+
+              //  System.out.println("too slow ! !");
+      //  }
+
+
+      // System.out.println("good driver");
+
+
+
+       //question 11
+
+
+       // System.out.println("What is your age Tomer?");
+
+     //   int tomerAge = s.nextInt();
+
+       // System.out.println( "What is your age danny ? ");
+       // int dannyAge = s.nextInt() ;
+
+      //  if (tomerAge >dannyAge)
+      //  {
+           //System.out.println("tomer is older ! !");
+
+       // }
+
+
+      //  if (tomerAge < dannyAge)
+      // {
+
+
+      //      System.out.println(" dannyage is older");
+
+       // }
+
+
+
+         //question 12
+
+        /* System.out.println("num 1:");
+
+        int num_1 =s.nextInt();
+
+        System.out.println("num 2:");
+
+        int num_2 =s.nextInt();
+
+        if (num_1 == num_2)
+        {
+            System.out.println("equal");
+
+        }
+
+
+        if (num_1 < num_2)
+        {
+            System.out.println(num_1);
+
+        }
+
+         if (num_1 > num_2)
+             {
+                 System.out.println(num_2);
+        }
+     */
+
+
+        //question 13
+
+       /* int num1,num2,num3;
+        System.out.println("Enter first number: ");
+        num1= s.nextInt();
+        System.out.println("Enter second number: ");
+        num2=s.nextInt();
+        System.out.println("Enter third number: ");
+        num3=s.nextInt();
+        if(num1<num2 && num1>num3 || num1>num2 && num1<num3)
+            System.out.println(num1);
+        else if(num2<num1 && num2>num3 || num2>num1 && num2<num3)
+            System.out.println(num2);
+        else if(num3<num1 && num3>num2 || num3>num1 && num3<num2)
+            System.out.println(num3);
+        else
+            System.out.println("There is no middle number.");
+
+
+       */
+
+
+        //question 14
+
+
+
+
+
+
+        // question 15
+      /* int x = 0;
+        while (x <=10)
+     {
+         System.out.println(x);
+       x++;
+
+     }
+    */
+
+
+
+       // question 16
+
+       /* int y = 1;
+      while (y <= 50)
+      {
+          System.out.println(y);
+
+          y = y + 2 ;
+      }
+     */
+        //question 17
+       /* int random_number;
+       do {
+            Random r = new Random();
+            random_number = r.nextInt(20) + 10;
+            System.out.println("The number that was chosen: " + random_number);
+        }while(random_number!=15);
+
+      */
+
+
+
+        //question 18
+
+       /* System.out.println("Please enter a number:");
+        int number =s.nextInt();
+
+            switch (number){
+                case 1:
+                    System.out.println("One");
+                    break;
+                case 2 :
+                    System.out.println("Two");
+                    break;
+
+                case 3:
+                    System.out.println("Three");
+                    break;
+
+                default:
+                    System.out.println("unkown number");
+                    break;
+
+
+            }
+         */
+
+
+
+        //question 19
+       /* int a,sum=0;
+        System.out.println("Enter number: ");
+        a=s.nextInt();
+        while(true)
+        {
+            if(a!=0) {
+                sum += a;
+                System.out.println("Enter number: ");
+                a = s.nextInt();
+            }
+            else
+                break;
+        }
+        System.out.println("Sum is: "+sum);
+
+        */
+        //question 20
+       /* int a;
+        System.out.println("Enter a number between 100-999: ");
+        a=s.nextInt();
+        while(a<100 || a>=1000)
+        {
+            System.out.println("Enter a number between 100-999: ");
+            a=s.nextInt();
+        }
+        System.out.println("The number is: "+ a/100);
+
+
+      */
+        //question 21
+
+
+
+
+
+
+        //question 22
+
+     /*   int a,sum=1;
+        System.out.println("Enter number: ");
+        a=s.nextInt();
+        for(int i=2;i<=a;i++)
+            sum*=i;
+        System.out.println("Solution: "+sum);
+      */
+
+
+      //question 23
+
+     /*int a,max=0;
+        System.out.println("Enter number: ");
+        a=s.nextInt();
+        while(a>0)
+        {
+            if(max<a)
+                max=a;
+            System.out.println("Enter number: ");
+            a=s.nextInt();
+        }
+        System.out.println("Max number is: "+max);
+       */
+
+
+     
